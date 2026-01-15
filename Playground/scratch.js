662779010c1efbeead9f2356cddd8a54cddfe557
wisdomlight <wisdomlight@protonmail.com>
2026-01-15 23:18:33 +0000

    pushing from vscode. I had enough with webstorm on Byte II not responding to keyboard

--- Playground/scratch.js
+++
@@ -1,7 +0,0 @@
-const talkBtn = document.getElementById('send-btn');
-const userText = document.getElementById('user-text');
-
-talkBtn.addEventListener('click', function(){
-    console.log(userText.value)
-    userText.value = '';
-})
--- index.js
+++ index.js
@@ -1,14 +1,43 @@
 import { tweetsData } from './data.js'
 const tweetBtn = document.getElementById('tweet-btn')
 const tweetInput = document.getElementById('tweet-input')
+const feed = document.getElementById('feed')
 tweetBtn.addEventListener('click', () => {
     console.log(tweetInput.value)
     tweetInput.value = '';
 })
+document.addEventListener('click', function(e){
+    if(e.target.dataset.like){
+        console.log(e.target.dataset.like)
+        handleLikeClick(e.target.dataset.like)
+    }
+})
+// this has been my solution to the challenge on the Like a tweet part 5: find the tweet object
+// function handleLikeClick(tweetId){
+//     console.log(tweetId)
+//     tweetsData.forEach(function(tweet){
+//         if(tweet.uuid === tweetId){
+//             const targetTweetObj = tweet
+//             tweet.likes += 1
+//             console.log(targetTweetObj)
+//         }
+//     })
+// }
+// The scrim is "Like a tweet part 5" I did my solution with forEach - and the tutor did it with filter!!! 
+// They searched for the object with the uuid and because there would be only one such object the could
+// safely say at the end [0]
+// this is the scrimba solution
+function handleLikeClikc(tweetId){
+    const targetTweetObj = tweetsData.filter(function(tweet){
+        return tweet.uuid === tweetId
+    })[0]
+    targetTweetObj.likes++
+    console.log(tweetsData)
 
+}
 function getFeedHtml(){
     let feedHtml = '';
-    for(let tweet of tweetsData){
+    tweetsData.forEach(function(tweet){
         feedHtml += `
                 <div class="tweet">
                     <img src="${tweet.profilePic}" class="profile-pic">
@@ -16,19 +45,22 @@ function getFeedHtml(){
                         <p class="handle">${tweet.handle}</p>
                         <p class="tweet-text">${tweet.tweetText}</p>
                         <div class="tweet-details">
-                            <span class="tweet-detail">${tweet.replies.length}</span>
-                            <span class="tweet-detail">${tweet.likes}</span>
-                            <span class="tweet-detail">${tweet.retweets}</span>
+                            <span class="tweet-detail">
+                            <i class="fa-regular fa-comment-dots" data-reply="${tweet.uuid}"></i>${tweet.replies.length}</span>
+                            <span class="tweet-detail"><i class="fa-solid fa-heart" data-like="${tweet.uuid}" ></i>${tweet.likes}</span>
+                            <span class="tweet-detail"><i class="fa-solid fa-retweet" data-retweet="${tweet.uuid}"></i>${tweet.retweets}</span>
 
                         </div>
                     </div>
                 </div>
         `
-    }
+    })
     return feedHtml;
 }
-getFeedHtml();
-
+function render(){
+    feed.innerHTML = getFeedHtml();
+}
+render()
 /*
 <div class="tweet">
     <img src="URL OF PROFILE PIC" class="profile-pic">
